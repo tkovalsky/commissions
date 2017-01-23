a539c914db40bab21e07f751de4d80d37f59409a
todd <t.e.kovalsky@gmail.com>
2017-01-22 22:21:02 -0500

    updated the tables on the index template

--- commissions/urls.py
+++ commissions/urls.py
@@ -4,12 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-#    url(r'^contacts/$', views.ContactListView.as_view(), name='contacts'),
-#    url(r'^contact/(?P<pk>\d+)$', views.ContactDetailView.as_view(), name='contact-detail'),
-#    url(r'^leases/$', views.LeaseListView.as_view(), name='leases'),
-#    url(r'^lease/(?P<pk>[0-9]+)/$', views.LeaseDetailView.as_view(), name='lease-detail'),
-#    url(r'^deals/$', views.DealListView.as_view(), name='deals'),
-#    url(r'^sale/(?P<pk>[0-9]+)/$', views.DealDetailView.as_view(), name='deal-detail'),
+
     url(r'^sales/$', views.SalesListView.as_view(), name='sales'),
     url(r'^sale/(?P<pk>[0-9]+)/$', views.SaleDetailView.as_view(), name='sale-detail'),
     url(r'^sale/create/$', views.SaleCreateView.as_view(), name='sale-create'),
@@ -20,10 +15,8 @@ urlpatterns = [
     url(r'^lease/create/$', views.LeaseCreateView.as_view(), name='lease-create'),
     url(r'^lease/(?P<pk>\d+)/update/$', views.LeaseUpdateView.as_view(), name='lease-update'),
     url(r'^lease/(?P<pk>\d+)/delete/$', views.LeaseDeleteView.as_view(), name='lease-delete'),
-
-
     url(r'^location/create/$', views.AddLocationView.as_view(), name='location-create'),
     url(r'^location/(?P<pk>[0-9]+)/$', views.LocationDetailView.as_view(), name='location-detail'),
     url(r'^locations/$', views.LocationListView.as_view(), name='locations'),
-    
+
 ]
